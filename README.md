99b07e9b874a05b1ea7ef7ea2d6649c61937bb49
João Cordeiro <jsantanacord@gmail.com>
2026-07-13 02:26:57 -0300

    chore: configura infraestrutura inicial do banco de dados

--- README.md
+++ README.md
@@ -15,20 +15,4 @@ Case técnico - Desenvolvedor(a) Full-Stack Júnior - Keener.
 (em construção)
 
 ## Justificativa das escolhas técnicas
-(em construção)# Controle de Estoque
-
-Sistema simples de controle de estoque para uma pequena empresa de varejo.
-
-## Stack
-- Backend: C# (.NET 8) com ASP.NET Core
-- Frontend: React
-- Banco de dados: PostgreSQL
-
-## Como rodar localmente
-(em construção)
-
-## Variáveis de ambiente
-(em construção)
-
-## Justificativa das escolhas técnicas
 (em construção)
--- backend/ControleEstoque.Api/Program.cs
+++ backend/ControleEstoque.Api/Program.cs
@@ -1,9 +1,16 @@
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?.Replace("SUBSTITUIR", Environment.GetEnvironmentVariable("DB_PASSWORD"));
 
 var app = builder.Build();
 
